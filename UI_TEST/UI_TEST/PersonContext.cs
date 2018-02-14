7a836d31758dbdfd62d0a5a22b1281a71549d664
Milan Dierick <milan.dierick@gmail.com>
2018-02-14 15:27:23 +0100

    Implemented INotifyPropertyChanged, Greeting does not update.

--- UI_TEST/UI_TEST/PersonContext.cs
+++ UI_TEST/UI_TEST/PersonContext.cs
@@ -1,13 +1,44 @@
 ﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using UI_TEST.Annotations;
 
 namespace UI_TEST
 {
-    class PersonContext : INotifyPropertyChanged
+    internal class PersonContext : INotifyPropertyChanged
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Greeting { get; }
+        private string _firstName;
+        private string _lastName;
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (value == _firstName) return;
+                _firstName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (value == _lastName) return;
+                _lastName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Greeting => $"Hello {_firstName} {_lastName}";
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
