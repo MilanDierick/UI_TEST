93eeba630d69c4dbe51e88a008cea53254a1df99
Milan Dierick <milan.dierick@gmail.com>
2018-02-14 14:53:30 +0100

    First version, without ReactiveUI, does not update the Greeting message yet.

---
+++ UI_TEST/UI_TEST/PersonContext.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace UI_TEST
+{
+    class PersonContext : INotifyPropertyChanged
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Greeting { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
