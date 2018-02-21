99bb1835a9b22ef4a28bb224a6959dcf5483bb8e
Milan Dierick <milan.dierick@gmail.com>
2018-02-21 10:30:42 +0100

    Temp

--- UI_TEST/UI_TEST/Contexts/PopulationContext.cs
+++ UI_TEST/UI_TEST/Contexts/PopulationContext.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reflection;
 using ReactiveUI;
 
 namespace UI_TEST
@@ -6,20 +10,24 @@ namespace UI_TEST
     public class PopulationContext : ReactiveObject
     {
         private PersonContext _selection;
+        private PersonContext _selectedFriend;
 
         public PopulationContext()
         {
-            People = new ReactiveList<PersonContext>
-            {
-                new PersonContext(),
-                new PersonContext(),
-                new PersonContext(),
-                new PersonContext(),
-                new PersonContext()
-            };
+            People = new ReactiveList<PersonContext>();
+
+            this.AddFriend = ReactiveCommand.Create(
+                () => { People.Add(new PersonContext()); }
+            );
+            this.RemoveFriend = ReactiveCommand.Create(
+                () => { People.Remove(Selection)) });
 
             Selection = People.FirstOrDefault();
         }
+
+        public ReactiveCommand<Unit, Unit> AddFriend { get; }
+
+        public ReactiveCommand<Unit, Unit> RemoveFriend { get; }
 
         public ReactiveList<PersonContext> People { get; }
 
@@ -27,6 +35,12 @@ namespace UI_TEST
         {
             get => _selection;
             set => this.RaiseAndSetIfChanged(ref _selection, value);
+        }
+
+        public PersonContext SelectedFriend
+        {
+            get => _selectedFriend;
+            set => this.RaiseAndSetIfChanged(ref _selectedFriend, value);
         }
     }
 }
